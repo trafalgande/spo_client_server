14129ce7fcb527f4dc27bc4b498c9d85c50d495a
trafalgande <thepicvsso@gmail.com>
2021-06-09 16:38:57 +0300

    Initial commit

---
+++ client.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define HOST "127.0.0.1"
+#define PORT 4444
+
+#define BUFFER_SIZE 1024
+
+int main() {
+
+    int clientSocket, ret;
+    struct sockaddr_in serverAddr;
+    char buffer[BUFFER_SIZE];
+
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket < 0) {
+        printf("[-] Error in connection.\n");
+        exit(1);
+    }
+    printf("[+] Client Socket is created.\n");
+
+    memset(&serverAddr, '\0', sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr(HOST);
+
+    ret = connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+    if (ret < 0) {
+        printf("[-] Error in connection.\n");
+        exit(1);
+    }
+    printf("[+] Connected to Server.\n");
+
+    while (1) {
+        printf("$ \t");
+        if (fgets(buffer, sizeof(buffer), stdin)) {
+            buffer[strcspn(buffer, "\n")] = 0; // remove newline symbol
+            send(clientSocket, buffer, strlen(buffer), 0);
+        }
+
+
+        if (strcmp(buffer, ":exit") == 0) {
+            close(clientSocket);
+            printf("[-] Disconnected from server.\n");
+            exit(1);
+        }
+
+        if (recv(clientSocket, buffer, 1024, 0) < 0) {
+            printf("[-] Error in receiving data.\n");
+        } else {
+            printf(">> \t%s\n", buffer);
+        }
+    }
+    close(clientSocket);
+    return 0;
+}
