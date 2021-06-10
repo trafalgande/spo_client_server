7249e0c0179ee1baf6abbaadb630799e92759b61
topores <topores@inbox.ru>
2021-06-10 05:03:14 +0300

    Update api.c

--- api.c
+++ api.c
@@ -17,6 +17,7 @@ void init_db() {
     bson_append_utf8(&parent, "pizda_key",-1,"123",-1);
     bson_init(&child);
     bson_append_utf8(&child, "pizda_key",-1,"123",-1);
+    bson_append_utf8(&child, "pizda_key23",-1,"12323",-1);
     bson_append_document(&parent, "root",-1,&child);
     //bson_append_document_begin(&parent,"root1", -1, &child);
     //bson_append_utf8(&child, "pizda_key",-1,"12",-1);
@@ -32,7 +33,6 @@ void init_db() {
     }
 
     fwrite(&parent, sizeof(parent), 1, fptr);
-    fclose(fptr);
     fclose(fptr);
 
 }
@@ -50,7 +50,10 @@ bson_t read_bson() {
     bson_t b;
 
     fread(&b, sizeof(b), 1, fptr);
+
     fclose(fptr);
+
+
     return b;
 }
 
@@ -70,8 +73,64 @@ void write_bson(bson_t b) {
 
 char *api_create(char *path, char *param, char *value, char *cond) {
     bson_t b = read_bson();
-    bson_iter_t iter;
 
+    bson_iter_t iter;
+    bson_iter_t baz;
+    int path_len= strlen(param);
+    char path_t[path_len];
+    strcpy(path_t, path);
+    //printf("%s\n",path_t);
+    char * new_path = strtok(path_t, ":");
+    printf("%s\n",new_path);
+    char * key = strtok(NULL, ":");
+    printf("%s\n",key);
+    int full_path_len= strlen(new_path)+1+strlen(key);
+
+    char full_path[full_path_len];
+    strcpy(full_path, new_path);
+    //full_path1 = strcat(new_path,".");
+    strcat(full_path,".");
+    strcat(full_path,key);
+
+    printf("%s\n",full_path);
+
+
+
+    if (!(bson_iter_init(&iter, &b) && bson_iter_find_descendant(&iter, new_path, &baz))) return "{\"error\": \"path_is_invalid\"}";
+    if (bson_iter_init(&iter, &b) && bson_iter_find_descendant(&iter, full_path, &baz)) return "{\"error\": \"already_exists\"}";
+
+    bson_t* b_res;
+
+    uint32_t object_len;
+    const uint8_t *object_buf;
+
+    bson_iter_document(&baz, &object_len, &object_buf);
+    //bson_init_static(b_res,baz,object_len);
+    b_res = bson_new_from_data(object_buf, object_len);
+    if (strcmp(param, "dir") == 0){
+        bson_t node;
+        bson_init(&node);
+        bson_append_document(b_res, key,-1,&node);
+    } else if (strcmp(param, "node") == 0){
+        bson_t node;
+        bson_init(&node);
+        bson_append_utf8(b_res, key,-1,value,-1);
+    }
+
+    //baz=b_res;
+//    printf("%s\n\n\n", bson_as_canonical_extended_json(b_res,NULL));
+//
+//
+//    bson_t a;
+//
+//    bson_init(&a);
+//    bson_append_utf8(&a, "pizda_key",-1,"123",-1);
+
+    write_bson(b);
+
+
+    return "{\"status\": \"ok\"}";
+    //else
 
 }
 
@@ -81,6 +140,8 @@ const char *api_read(char *path, char *param, char *value, char *cond) {
     //printf("%s\n", bson_as_canonical_extended_json(&b,NULL));
     bson_iter_t iter;
     bson_iter_t baz;
+    if ((strcmp(path, "") == 0)||(strcmp(path, ".") == 0)) return bson_as_canonical_extended_json(&b,NULL);
+
     if (bson_iter_init(&iter, &b) && bson_iter_find_descendant(&iter, path, &baz)){
         if (BSON_ITER_HOLDS_UTF8 (&baz)) {
             //bson_iter_value (&baz)
@@ -110,4 +171,120 @@ const char *api_read(char *path, char *param, char *value, char *cond) {
 
 
 }
+
+
+char * api_update(char *path, char *param, char *value, char *cond){
+    bson_t b = read_bson();
+
+    bson_iter_t iter;
+    bson_iter_t baz;
+    int path_len= strlen(param);
+    char path_t[path_len];
+    strcpy(path_t, path);
+    //printf("%s\n",path_t);
+    char * new_path = strtok(path_t, ":");
+    printf("%s\n",new_path);
+    char * key = strtok(NULL, ":");
+    printf("%s\n",key);
+    int full_path_len= strlen(new_path)+1+strlen(key);
+
+    char full_path[full_path_len];
+    strcpy(full_path, new_path);
+    //full_path1 = strcat(new_path,".");
+    strcat(full_path,".");
+    strcat(full_path,key);
+
+    printf("%s\n",full_path);
+
+
+
+    if (!(bson_iter_init(&iter, &b) && bson_iter_find_descendant(&iter, new_path, &baz))) return "{\"error\": \"path_is_invalid\"}";
+    if (!(bson_iter_init(&iter, &b) && bson_iter_find_descendant(&iter, full_path, &baz))) return "{\"error\": \"don't_exists\"}";
+
+    bson_t* b_res;
+
+    uint32_t object_len;
+    const uint8_t *object_buf;
+
+    bson_iter_document(&baz, &object_len, &object_buf);
+    //bson_init_static(b_res,baz,object_len);
+    b_res = bson_new_from_data(object_buf, object_len);
+    if (strcmp(param, "dir") == 0){
+        //delete
+        bson_t node;
+        bson_init(&node);
+        bson_append_document(b_res, key,-1,&node);
+    } else if (strcmp(param, "node") == 0){
+        //delete
+        bson_t node;
+        bson_init(&node);
+        bson_append_utf8(b_res, key,-1,value,-1);
+    }
+
+    write_bson(b);
+
+
+    return "{\"status\": \"ok\"}";
+}
+
+
+char * api_delete(char *path, char *param, char *value, char *cond){
+    bson_t b = read_bson();
+
+    bson_iter_t iter;
+    bson_iter_t baz;
+    int path_len= strlen(param);
+    char path_t[path_len];
+    strcpy(path_t, path);
+    //printf("%s\n",path_t);
+    char * new_path = strtok(path_t, ":");
+    printf("%s\n",new_path);
+    char * key = strtok(NULL, ":");
+    printf("%s\n",key);
+    int full_path_len= strlen(new_path)+1+strlen(key);
+
+    char full_path[full_path_len];
+    strcpy(full_path, new_path);
+    //full_path1 = strcat(new_path,".");
+    strcat(full_path,".");
+    strcat(full_path,key);
+
+    printf("%s\n",full_path);
+
+
+
+    if (!(bson_iter_init(&iter, &b) && bson_iter_find_descendant(&iter, new_path, &baz))) return "{\"error\": \"path_is_invalid\"}";
+    if (bson_iter_init(&iter, &b) && bson_iter_find_descendant(&iter, full_path, &baz)) return "{\"error\": \"already_exists\"}";
+
+    bson_t* b_res;
+
+    uint32_t object_len;
+    const uint8_t *object_buf;
+
+    bson_iter_document(&baz, &object_len, &object_buf);
+    //bson_init_static(b_res,baz,object_len);
+    b_res = bson_new_from_data(object_buf, object_len);
+    if (strcmp(param, "dir") == 0){
+        //хуй знает че тут бля писать
+
+    } else if (strcmp(param, "node") == 0){
+        //хуй знает че тут бля писать
+
+    }
+
+    //baz=b_res;
+//    printf("%s\n\n\n", bson_as_canonical_extended_json(b_res,NULL));
+//
+//
+//    bson_t a;
+//
+//    bson_init(&a);
+//    bson_append_utf8(&a, "pizda_key",-1,"123",-1);
+
+    write_bson(b);
+
+
+    return "{\"status\": \"ok\"}";
+}
+
 
